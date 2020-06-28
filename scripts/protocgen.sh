9bc3539361b8aa2f75e891123ae873cf1b2b44c8
Thunnini <yunjh1994@gmail.com>
2020-06-28 22:33:11 +0900

    Move proto file to root folder

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -7,9 +7,13 @@ cosmos_sdk_dir=$(go list -f '{{ .Dir }}' -m github.com/cosmos/cosmos-sdk)
 proto_dirs=$(find . -path ./third_party -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
   protoc \
-  -I. \
+  -I "proto" \
+  -I "third_party/proto" \
   -I "$cosmos_sdk_dir" \
-  --gocosmos_out=plugins=interfacetype,paths=source_relative,\
+  --gocosmos_out=plugins=interfacetype+grpc,\
 Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. \
   $(find "${dir}" -name '*.proto')
 done
+
+cp -r github.com/chainapsis/cosmos-sdk-interchain-account/* ./
+rm -rf github.com
