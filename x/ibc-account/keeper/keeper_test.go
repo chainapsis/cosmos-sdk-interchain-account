23886499daa66579e60068b2f06cdb93175751e2
Thunnini <yunjh1994@gmail.com>
2020-12-05 23:29:52 +0900

    Add test for query

--- x/ibc-account/keeper/keeper_test.go
+++ x/ibc-account/keeper/keeper_test.go
@@ -6,6 +6,8 @@ import (
 	"github.com/stretchr/testify/suite"
 
 	ibcacctesting "github.com/chainapsis/cosmos-sdk-interchain-account/x/ibc-account/testing"
+	"github.com/chainapsis/cosmos-sdk-interchain-account/x/ibc-account/types"
+	"github.com/cosmos/cosmos-sdk/baseapp"
 )
 
 type KeeperTestSuite struct {
@@ -15,12 +17,18 @@ type KeeperTestSuite struct {
 
 	chainA *ibcacctesting.TestChain
 	chainB *ibcacctesting.TestChain
+
+	queryClientA types.QueryClient
 }
 
 func (suite *KeeperTestSuite) SetupTest() {
 	suite.coordinator = ibcacctesting.NewCoordinator(suite.T(), 2)
 	suite.chainA = suite.coordinator.GetChain(ibcacctesting.GetChainID(0))
 	suite.chainB = suite.coordinator.GetChain(ibcacctesting.GetChainID(1))
+
+	queryHelperA := baseapp.NewQueryServerTestHelper(suite.chainA.GetContext(), suite.chainA.App.InterfaceRegistry())
+	types.RegisterQueryServer(queryHelperA, suite.chainA.App.IBCAccountKeeper)
+	suite.queryClientA = types.NewQueryClient(queryHelperA)
 }
 
 func TestKeeperTestSuite(t *testing.T) {
